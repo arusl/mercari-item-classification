6b0912b06db828d87b27fb7b07cdacae44ea4218
arusl <andrerusli19@gmail.com>
2021-05-06 21:18:46 +0900

    changed wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,6 @@
 from flask_apis import create_app
-if __name__ == "__main__":
-  app = create_app({'TESTING': True,})
-  app.run()
+# if __name__ == "__main__":
+#   app = create_app({'TESTING': True,})
+#   app.run()
+
+app = create_app({'TESTING': True,})
