ea537675f2a3816e83d62d9febed463e759d5bb1
Andre R <andrerusli19@gmail.com>
2021-05-07 01:02:53 +0900

    removed gunicorn usage in profcile

--- wsgi.py
+++ wsgi.py
@@ -1,6 +1,6 @@
 import os
 from flask_apis import create_app
-
-app = create_app({'TESTING': True,})
-port = int(os.environ.get('PORT', 5000))
-app.run(host='0.0.0.0', port=port)
+if __name__ == "__main__":
+  app = create_app({'TESTING': True,})
+  port = int(os.environ.get('PORT', 5000))
+  app.run(host='0.0.0.0', port=port)
